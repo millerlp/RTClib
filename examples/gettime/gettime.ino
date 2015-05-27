b487b5b9835b62d2735c896cb18c6d5ffc250db3
Luke Miller <millerlp@gmail.com>
2015-05-27 13:59:02 -0700

    Updated file extension to .ino, added some formatting to the tminute and seconds output to clean up the serial monitor output.

--- examples/gettime/gettime.ino
+++ examples/gettime/gettime.ino
@@ -1,12 +1,14 @@
-// Get date and time using a DS1307, DS1337 or DS1340 RTC connected via I2C
+// Get date and time using a DS3231, DS1307, DS1337 or DS1340 RTC connected via I2C
 
 
 #include <Wire.h>
 #include <SPI.h>  // not used here, but needed to prevent a RTClib compile error
 #include "RTClib.h"
 
-RTC_DS1307 RTC;    // Uncommnt this line if you are using the DS1307 chip or only use the basic functions of DS1337, DS1340, Chronodot (DS3231)
-
+// Create the RTC object. 
+// You may use RTC_DS1307 with the DS3231, DS1337, DS1340, Chronodot
+// because those clock chips all use the same basic addresses. 
+RTC_DS1307 RTC;    
 
 void setup () {
     Serial.begin(57600); // Set serial port speed
@@ -15,20 +17,26 @@ void setup () {
 }
 
 void loop () {
-    DateTime now = RTC.now(); // Read the time and date from the DS1340
+    DateTime now = RTC.now(); // Read the time and date from the clock chip
     
     Serial.print(now.year(), DEC);
-    Serial.print('/');
+    Serial.print('-');
     Serial.print(now.month(), DEC);
-    Serial.print('/');
+    Serial.print('-');
     Serial.print(now.day(), DEC);
     Serial.print(' ');
     Serial.print(now.hour(), DEC);
     Serial.print(':');
+	if (now.minute() < 10) {
+		Serial.print("0");
+	}
     Serial.print(now.minute(), DEC);
     Serial.print(':');
+	if (now.second() < 10) {
+		Serial.print("0");
+	}
     Serial.print(now.second(), DEC);
     Serial.println();
     
-    delay(3000);
+    delay(1000);
 }
